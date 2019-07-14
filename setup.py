1f12dcedcff197888fe085bd0e846e9985980644
AdamBlomfield <adam.blomfield@flatironschool.com>
2019-07-14 16:11:42 -0400

    Initialize repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using Machine Learning to analyze mixed martial arts fights in the UFC',
+    author='adam',
+    #license='',
+)
