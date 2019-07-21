c8585877fa95d51d644a34981e712cbe82b5f2f7
AdamBlomfield <adam.blomfield@flatironschool.com>
2019-07-21 15:42:36 -0400

    finished cleaning combined data

--- src/visualize.py
+++ src/visualize.py
@@ -41,3 +41,47 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
                     marker=markers[idx], 
                     label=cl, 
                     edgecolor='black')
+
+def column_distplot(dataframe, column, xlabel=None): 
+    '''Plot the histogram of a column using Seaborn'''
+    # Set Figure
+    sns.set(rc={'figure.figsize':(10,5)},style="white", context="talk")
+
+    # Plot
+    column = column
+    data = dataframe[column][~dataframe[column].isna()]
+    ax = sns.distplot(data);
+
+    # Title and Axis
+    ax.set_title("Histogram of the fighters' {}".format(column));
+    if type(xlabel)==str:
+        ax.set_xlabel(xlabel)
+    else:
+        ax.set_xlabel(str(column).capitalize())
+    sns.despine()
+    
+    col_cap = str(column).capitalize()
+    print("{} Skewness: {}".format(col_cap, round(dataframe[column].skew(), 2)))
+    print("{} Kurtosis: {}".format(col_cap, round(dataframe[column].kurt(), 2)))
+    
+    print('{} Mean: {}'.format(col_cap, round(data.mean(), 2)))
+    print('{} Median: {}'.format(col_cap, data.median()))
+    
+def column_countplot(dataframe, column, show_count=False):
+    '''Plot the count of each category for a column in the dataframe'''
+    
+    # Set the figure
+    sns.set(rc={'figure.figsize':(10,5)},style="white", context="talk")
+    
+    # Plot
+    ax = sns.countplot(x=column, data=dataframe, color="b")
+  
+    # Title and Axis
+    ax.set_title("Counts of each {} category".format(column))
+    ax.set_xlabel(str(column).capitalize())
+    ax.set_ylabel('Count')
+    sns.despine()
+    
+    if show_count:
+        for p in ax.patches:
+            ax.annotate('{:}'.format(p.get_height()), (p.get_x()+0.3, p.get_height()+10))
